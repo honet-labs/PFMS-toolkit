b6c12046ad67dfe9d1daeb10b344804a49e7f532
andriii <andri@honet.web.id>
2026-07-06 14:25:35 +0700

    Delete tools/snmp-bridge directory

--- tools/snmp-bridge/bootstrap/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use SnmpBridge\Database\ConnectionManager;
-use SnmpBridge\Database\PDOFactory;
-
-return new ConnectionManager(new PDOFactory(), require dirname(__DIR__) . '/config/database.php');
